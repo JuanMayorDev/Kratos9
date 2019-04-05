766b8dea186f423312c1dd849c6d91be30f5cd13
juan-mayor <juanmayor96@gmail.com>
2019-04-06 00:07:26 +0200

    Hierarchy folders

---
+++ KRATOS9/Assets/Scripts/SimpleForwardMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleForwardMovement : MonoBehaviour
+{
+
+    private Rigidbody my_rb;
+    private Transform my_tr;
+    public float movement_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        my_rb = GetComponent<Rigidbody>();
+
+        my_tr = GetComponent<Transform>();
+        my_rb.velocity = my_tr.forward * movement_speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
