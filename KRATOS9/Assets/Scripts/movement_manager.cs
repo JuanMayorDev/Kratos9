991145c703589c062a8e3b6220a7179cf1344492
Pokoi <jesusferminvillar@gmail.com>
2019-04-07 02:40:27 +0200

    520255252

---
+++ KRATOS9/Assets/Scripts/movement_manager.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kratos9 {
+    public class movement_manager : MonoBehaviour
+    {
+        
+        public Transform ship_transform;
+
+        public float   angles_to_rotate;
+        public Vector3 flow_direction;
+        public float   max_director_speed_magnitude;
+        public Vector3 director_speed;
+        public enum SideToRotate {left, right }
+        public bool hitting;
+        public float dash_speed;
+
+        public float base_speed    = 1f;
+        public float current_speed = 1f;
+
+        public float lerp_time;
+
+        punch_manager this_punch_manager;
+
+        public Animator my_anim;
+
+        Quaternion desired_rotation;
+
+        public float rotation_speed;
+
+        public Transform direction_shadow;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            desired_rotation = ship_transform.rotation;
+            my_anim = transform.GetChild(0).GetChild(0).GetComponent<Animator>();
+            director_speed = ship_transform.forward * base_speed;
+            this_punch_manager = GetComponent<punch_manager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.DrawLine(ship_transform.position, ship_transform.position + director_speed * 10, Color.blue);
+            ship_transform.position += (director_speed * current_speed) * Time.deltaTime;
+
+            if (!hitting)
+            {
+                director_speed = director_speed.magnitude > 0 ? director_speed - director_speed * Time.deltaTime * 0.1f : Vector3.zero;
+
+                if(director_speed != flow_direction)
+                {
+                    director_speed += flow_direction * Time.deltaTime;
+                }
+                director_speed = Vector3.ClampMagnitude(director_speed, max_director_speed_magnitude);
+            }
+            ship_transform.rotation = Quaternion.Lerp(ship_transform.rotation, desired_rotation, Time.deltaTime * rotation_speed);
+
+            direction_shadow.forward = Vector3.Lerp(direction_shadow.forward, director_speed, Time.deltaTime);
+        }
+
+        public void RotateShip(SideToRotate _s, float angles)
+        {
+           switch (_s)
+            {
+                case SideToRotate.left:
+
+                    my_anim.SetTrigger("Left");
+
+                    director_speed = Quaternion.AngleAxis(-angles, Vector3.up) * director_speed;
+                    desired_rotation.eulerAngles = new Vector3(ship_transform.eulerAngles.x, ship_transform.eulerAngles.y - angles, ship_transform.eulerAngles.z);
+                    //ship_transform.eulerAngles = new Vector3(ship_transform.eulerAngles.x, ship_transform.eulerAngles.y - angles, ship_transform.eulerAngles.z);
+                    break;
+
+                case SideToRotate.right:
+                    my_anim.SetTrigger("Right");
+
+                    director_speed = Quaternion.AngleAxis(+angles, Vector3.up) * director_speed;
+                    desired_rotation.eulerAngles = new Vector3(ship_transform.eulerAngles.x, ship_transform.eulerAngles.y + angles, ship_transform.eulerAngles.z);
+
+
+                    //ship_transform.eulerAngles = new Vector3(ship_transform.eulerAngles.x, ship_transform.eulerAngles.y - angles, ship_transform.eulerAngles.z);
+                    break;
+
+              
+            }
+        }
+
+        public void UpdateDirectorVector(Vector3 _v)
+        {
+            director_speed += _v  ;
+
+        }
+
+        public void IncreaseSpeed(float _s)
+        {
+             if(!hitting) UpdateDirectorVector(ship_transform.forward * _s);            
+        }
+
+        public Vector3 GetDirectorVector()
+        {
+            return director_speed;
+        }
+
+        public float GetSpeed()
+        {
+            return current_speed;
+        }
+
+        public void DecreaseSpeed(float _s)
+        {
+            if (!hitting) UpdateDirectorVector(-ship_transform.forward * _s * 5f);          
+        }
+
+        public void RecieveImpact(Vector3 _v)
+        {
+            director_speed += _v;
+        }
+
+       
+    }
+}
---
+++ KRATOS9/Assets/Scripts/punch_manager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kratos9
+{
+    public class punch_manager : MonoBehaviour
+    {
+
+        movement_manager this_movement_manager;
+
+        public byte punch_charge_needed;
+        public byte punch_charge;
+        public float punch_dash_duration;
+        public bool punch_ready;
+        public PunchButtonController my_punch_button_controller;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            this_movement_manager = GetComponent<movement_manager>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) Punch();
+        }
+
+        public void Punch()
+
+        { 
+            if(punch_ready)
+            {
+                this_movement_manager.my_anim.SetTrigger("Attack");
+                this_movement_manager.hitting = true;
+                this_movement_manager.director_speed = Vector3.zero;
+                this_movement_manager.RecieveImpact(this_movement_manager.ship_transform.forward * this_movement_manager.dash_speed);
+                punch_charge = 0;
+                punch_ready = false;
+                Invoke("StopPunchEffect", punch_dash_duration);
+                my_punch_button_controller.ResetFillAmount();
+            }
+
+
+        }
+
+        public void StopPunchEffect()
+        {
+            CancelInvoke("StopPunchEffect");
+            this_movement_manager.director_speed = Vector3.forward;
+            this_movement_manager.current_speed = this_movement_manager.base_speed;
+            this_movement_manager.hitting = false;
+        }
+
+        public void IncreasePunchCharge(byte _b)
+        {
+            punch_charge += _b;
+            float amount = (float)punch_charge / punch_charge_needed;
+            my_punch_button_controller.UpdateFillAmount(amount);
+        }
+
+    }
+}
